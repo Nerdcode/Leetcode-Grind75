780e5a2022d1883cd76403eb30271b41e8f20956
durgaprasad.kusuma <durgaprasad.kusuma@servicenow.com>
2023-10-01 23:11:51 +0530

    solution for invert bionary tree in python

---
+++ Python/06_Invert_binary_tree/06_Invert_binary_tree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: #Base Case
+            return root
+        self.invertTree(root.left) #Call the left substree
+        self.invertTree(root.right)  #Call the right substree
+        # Swap the nodes
+        root.left, root.right = root.right, root.left
+        return root # Return the root
