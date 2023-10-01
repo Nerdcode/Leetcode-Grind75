4c96f68ca00e9191343528cf9e83e764c1ac3dfd
durgaprasad.kusuma <durgaprasad.kusuma@servicenow.com>
2023-10-01 22:55:05 +0530

    Solution for valid palindrome

---
+++ Python/05_Valid_palindrome/05_Valid_palindrome.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
