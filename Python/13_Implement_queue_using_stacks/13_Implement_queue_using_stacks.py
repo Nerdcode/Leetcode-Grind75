599892213c84b2481d47a0e670021bb31b09958a
Aryan Malik <88618913+Aryan9592@users.noreply.github.com>
2023-10-02 17:42:33 +0530

    Update 13_Implement_queue_using_stacks.py

---
+++ Python/13_Implement_queue_using_stacks/13_Implement_queue_using_stacks.py
@@ -0,0 +1,20 @@
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        self.stack1.append(x)
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+
+    def pop(self) -> int:
+        return self.stack1.pop()
+
+    def peek(self) -> int:
+        return self.stack1[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack1) == 0
