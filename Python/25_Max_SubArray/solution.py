f9184721f626a1cd3eb1bd2f9cc5a665dffff598
Samihan <samihannandedkar15@gmail.com>
2023-10-09 17:43:40 +0530

    completed issue: #50

---
+++ Python/25_Max_SubArray/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = float('-inf')
+        currentSum = 0
+        
+        for num in nums:
+            currentSum += num
+            
+            if currentSum > maxSum:
+                maxSum = currentSum
+            
+            if currentSum < 0:
+                currentSum = 0
+        
+        return maxSum
